14d7a3b7ef2dbdee0a8a4e424f695c3e31e079b7
InfestedHydra <InfestedHydra@gmail.com>
2014-12-14 14:49:50 +0200

    Session/connection tracking
    temp test index.php

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
+<?php
+if (session_status() == PHP_SESSION_NONE)
+{
+session_start();
+echo session_id();
+
+}
+
+
+
+?>
+
 <!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
 <link rel="stylesheet" type="text/css" href="css/global.css">
 <title>Our title here</title>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/global.js"></script>
 </head>
-<body>
+<body  onLoad="jsInit()">
 <div>
 Hello World!
+	<div id="table">
+	</div>
 </div>
 </body>
---
+++ js/global.js
@@ -0,0 +1,11 @@
+function jsInit()
+{
+}
+
+function jsUpdate()
+{
+}
+
+function jsGetState()
+{
+}
